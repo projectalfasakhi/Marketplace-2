1b470b665e51aad60141c9b7254e36ab20506597
Rasyidin17 <maduhur101@gmail.com>
2023-09-22 16:10:45 +0700

    multi auth

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,99 @@
 <?php
 
+  
+
 use Illuminate\Support\Facades\Route;
 
+  
+
+use App\Http\Controllers\HomeController;
+
+  
+
 /*
+
 |--------------------------------------------------------------------------
+
 | Web Routes
+
 |--------------------------------------------------------------------------
+
 |
+
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+
+| routes are loaded by the RouteServiceProvider within a group which
+
+| contains the "web" middleware group. Now create something great!
+
 |
+
 */
 
+  
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Auth::routes();
+
+  
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Normal Users Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+
+  
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+});
+
+  
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+
+  
+
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+
+});
+
+  
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:manager'])->group(function () {
+
+  
+
+    Route::get('/manager/home', [HomeController::class, 'managerHome'])->name('manager.home');
+
 });
